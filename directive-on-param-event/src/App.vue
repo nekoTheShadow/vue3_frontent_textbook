46988573f0ae963d6af10ea5e6322bb89a169a6d
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 13:27:01 +0900

    4.2.3 メソッドの引数としてイベントオブジェクトを受け取る

---
+++ directive-on-param-event/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const mousePointX = ref(0);
+const mousePointY = ref(0);
+const onImgMousemove = (event: MouseEvent) => {
+  mousePointX.value = event.offsetX;
+  mousePointY.value = event.offsetY;
+}
+</script>
+
+<template>
+  <section>
+    <img src="./assets/logo.svg" alt="Vueのロゴ" width="200" v-on:mousemove="onImgMousemove"></img>
+    <p>ポインタの位置: x={{ mousePointX }} y={{ mousePointY }}</p>
+  </section>
+</template>
+
+<style scoped></style>

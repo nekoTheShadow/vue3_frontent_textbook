ba08ebd8418f2488d2dc18ad843910231a53b4b2
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 19:06:05 +0900

    6.1.4 v-ifと似て非なるv-show

---
+++ directive-show/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const number = ref(80);
+const showOrNot = computed(() => Math.round(Math.random() * 100) >= 50);
+</script>
+
+<template>
+  <section>
+    v-ifを利用
+    <p v-if="showOrNot">条件に合致したので表示</p>
+  </section>
+  <section>
+    v-showを利用
+    <p v-show="showOrNot">条件に合致したので表示</p>
+  </section>
+</template>
+
+<style scoped></style>

ef6ba21d7861b08ca0e2b63e6de4ed290fe5fb3b
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 18:49:36 +0900

    6.1.1 条件分岐ディレクティブの基本v-if

---
+++ directive-conditional-basic/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const number = ref(80);
+const showOrNot = computed(() => Math.round(Math.random() * 100) >= 50);
+</script>
+
+<template>
+  <p v-if="number >= 50">
+    条件に合致したので表示①
+  </p>
+  <p v-if="Math.round(Math.random() * 100) >= 50">
+    条件に合致したので表示②
+  </p>
+  <p v-if="showOrNot">
+    条件に合致したので表示③
+  </p>
+</template>
+
+<style scoped></style>

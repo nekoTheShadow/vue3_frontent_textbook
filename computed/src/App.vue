5b45cc096eda02a9e30c9348e21b7078bd045734
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 12:23:59 +0900

    3.3　リアクティブデータ用意のバリエーション

---
+++ computed/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {ref, computed} from "vue";
+
+const radiusInit = Math.round(Math.random() * 10);
+const PI = ref(3.14);
+const radius = ref(radiusInit);
+const area = computed(() => radius.value * radius.value * PI.value);
+setInterval(() => radius.value = Math.round(Math.random() * 10), 1000);
+</script>
+
+<template>
+  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
+</template>
+
+<style scoped></style>

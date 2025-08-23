e998d4964c38d55a9593697cf516cdbb45eee467
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 15:42:33 +0900

    5.1.3 v-bindとv-on:inputの組み合わせ

---
+++ directive-twoway/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const inputName2Way = ref("双方向");
+const onInputName2Way = (event: Event) => {
+  const element = event.target as HTMLInputElement;
+  inputName2Way.value = element.value;
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way"></input>
+    <p>{{ inputName2Way }}</p>
+  </section>
+</template>
+
+<style scoped></style>

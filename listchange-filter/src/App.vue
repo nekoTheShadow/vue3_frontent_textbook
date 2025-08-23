c91e94154382ba80b0922c7acf96943de0089cdd
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 19:59:24 +0900

    6.3.1 ループ対象データの絞り込み

---
+++ listchange-filter/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+interface Cocktail {
+  id: number, 
+  name: string,
+  price: number
+}
+
+const cocktailDataListInit: Cocktail[] = [
+  {id: 2345, name: "ホワイトレディ", price: 1200},
+  {id: 4412, name: "ブルーハワイ", price: 1500},
+  {id: 6792, name: "ニューヨーク", price: 1100},
+  {id: 8429, name: "マティーニ", price: 1200}
+]
+const cocktailDataList = ref(cocktailDataListInit);
+const cocktail1500 = computed(() => cocktailDataList.value.filter((cocktailItem) => cocktailItem.price == 1500));
+</script>
+
+<template>
+  <section>
+    すべてのカクテルリスト
+    <ul>
+      <li v-for="cocktailItem in cocktailDataList" v-bind:key="cocktailItem.id">{{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円</li>
+    </ul>
+  </section>
+
+  <section>
+    値段が1500円のカクテルリスト
+    <ul>
+      <li v-for="cocktailItem in cocktail1500" v-bind:key="cocktailItem.id">{{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円</li>
+    </ul>
+  </section>
+</template>
+
+<style scoped></style>
+

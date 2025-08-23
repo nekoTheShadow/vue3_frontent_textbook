236f4b478099e37ed42d1ec8ad7a2ac2019c68a8
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 16:50:44 +0900

    5.2.1 HTML文字列をそのまま表示するv-html

---
+++ directive-html/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const htmlStr = ref(`<a href="https://vuejs.org">Vue.jsのTOPページ</a>`);
+</script>
+
+<template>
+  <section>{{ htmlStr }}</section>
+  <section v-html="htmlStr"></section>
+</template>
+
+<style scoped></style>

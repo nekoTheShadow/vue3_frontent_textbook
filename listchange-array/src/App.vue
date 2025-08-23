285008dcc4923cd5e43ac11837c42d52a52c1165
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 20:06:01 +0900

    6.3.2 配列のデータ操作

---
+++ listchange-array/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const cocktailListInit = ["ホワイトレディ", "ブルーハワイ", "ニューヨーク"];
+const cocktailList = ref(cocktailListInit);
+const changeCocktailList = () => cocktailList.value = ["バラライカ", "XYZ", "マンハッタン"];
+const addCocktailList = () => cocktailList.value.push("ブルームーン");
+const deleteFromCocktailList = () => cocktailList.value.pop();
+</script>
+
+<template>
+  <ul>
+    <li v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">{{ cocktailName }}（インデックス：{{ index }}）</li>
+  </ul>
+  <p>CocktailListを<button v-on:click="changeCocktailList">変更</button></p>
+  <p>CocktailListの末尾に「ブルームーン」を<button v-on:click="addCocktailList">追加</button></p>
+  <p>CocktailListから末尾の要素を<button v-on:click="deleteFromCocktailList">削除</button></p>
+</template>
+
+<style scoped></style>

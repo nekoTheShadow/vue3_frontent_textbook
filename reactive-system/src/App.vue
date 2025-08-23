89cf5525983f0f291ad71b800b299e730e79f404
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 12:13:03 +0900

    3.2　リアクティブシステム

---
+++ reactive-system/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// https://gihyo.jp/book/2022/978-4-297-13072-5/support
+
+import {ref} from "vue";
+
+const now = new Date();
+const nowStr = now.toLocaleTimeString();
+let timeStr = nowStr;
+const timeStrRef = ref(nowStr);
+
+function changeTime(): void {
+  const newTime = new Date();
+  const newTimeStr = newTime.toLocaleTimeString();
+  timeStr = newTimeStr;
+  timeStrRef.value = newTimeStr;
+}
+
+setInterval(changeTime, 1000);
+</script>
+
+<template>
+  <p>現在時刻：{{ timeStr }}</p>
+  <p>現在時刻（ref）：{{ timeStrRef }}</p>
+</template>
+

4583dc35ba985b20b32ef7ec396874e45e927661
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 19:33:40 +0900

    6.2.4 オブジェクトのループ

---
+++ directive-loop-obj/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const whiteLadyInit = {
+  id: 2345,
+  name: "ホワイトレディ",
+  price: 1200,
+  recipe: "ジン30ml+コワントロー15ml+レモン果汁15ml"
+}
+const whiteLady = ref(whiteLadyInit);
+</script>
+
+<template>
+  <dl>
+    <template v-for="(value, key) in whiteLady" v-bind="key">
+      <dt>{{ key }}</dt>
+      <dl>{{ value }}</dl>
+    </template>
+  </dl>
+</template>
+
+<style scoped></style>

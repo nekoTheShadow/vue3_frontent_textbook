2420f49a6c188af9446eefdf3455d365ded559bc
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 16:10:59 +0900

    5.1.5 v-modelの修飾子

---
+++ directive-model-trim/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const trimedText = ref("");
+</script>
+
+<template>
+  <section>
+    <input type="text" v-model.trim="trimedText"></input>
+    <p>入力文字列:{{ trimedText }}</p>
+  </section>
+</template>
+
+<style scoped></style>

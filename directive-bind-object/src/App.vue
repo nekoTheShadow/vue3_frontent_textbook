5c7c5b73c85276cd52604f9771d893aa02d8dfea
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 13:11:36 +0900

    4.1.5 複数の属性にまとめてバインドする方法

---
+++ directive-bind-object/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const imgAtributes = ref({
+  src:"/images/logo.svg",
+  alt:"Vueのロゴ",
+  width:75,
+  height:75
+});
+</script>
+
+<template>
+  <p><img v-bind="imgAtributes"></img></p>
+  <p><img v-bind="imgAtributes" title="ロゴです！"></img></p>
+  <p><img v-bind="imgAtributes" alt="ロゴです！"></img></p>
+</template>
+
+<style scoped></style>

4762b03e9d3635f4e9a1923f13f4a10891e681a4
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 13:11:04 +0900

    4.1.4 バインドする属性をテンプレート変数として指定

---
+++ directive-bind-dynamic/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const widthOrHeight = ref("height");
+const widthOrHeightValue = ref(100);
+</script>
+
+<template>
+  <p><img alt="VueLogo" src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue"></img></p>
+</template>
+
+<style scoped></style>

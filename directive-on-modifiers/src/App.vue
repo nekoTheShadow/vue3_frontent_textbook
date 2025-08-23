53916134dc010a901c6445cf1377623f04ab3f55
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 15:26:29 +0900

    4.2.7 クリックイベントとキーイベントの修飾子

---
+++ directive-on-modifiers/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const msg = ref("まだです");
+const onEnterKey = () => msg.value = "エンターキーが押下されました";
+const onRightButtonClick = () => msg.value = "ボタンが右クリックされました";
+const onShiftClick = () => msg.value = "シフトを押しながらクリックされました";
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey"></input>
+  <br></br>
+  <button v-on:click.right="onRightButtonClick">右クリック</button>
+  <br></br>
+  <button v-on:click.shift="onShiftClick">シフトを押しながらクリック</button>
+</template>
+
+<style scoped></style>

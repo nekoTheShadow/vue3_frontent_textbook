e812a9bd6d255e8bb106503e16ea01846d423344
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 15:18:01 +0900

    4.2.6 v-onの修飾子

---
+++ directive-on-prevent/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const msg = ref("未送信");
+const onFormSubmit = () => msg.value = "送信されました";
+</script>
+
+<template>
+  <form action="#" v-on:submit.prevent="onFormSubmit">
+    <input type="text" required></input>
+    <button type="submit">送信</button>
+  </form>
+  <p>{{ msg }}</p>
+</template>
+
+<style scoped></style>

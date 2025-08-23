7ed194c72563895b259dca2b118affb85d5e944f
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 13:19:30 +0900

    4.2.1 イベントリスナを設定するディレクティブv-on

---
+++ directive-on-basic/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const randValue = ref("まだです");
+const onButtonClick = () => {
+  const rand = Math.round(Math.random() * 10);
+  randValue.value = String(rand);
+}
+</script>
+
+<template>
+  <section>
+    <button v-on:click="onButtonClick">クリック！</button>
+    <p>クリックの結果：{{ randValue }}</p>
+  </section>
+</template>
+
+<style scoped></style>

7732030d7f6056bc496b93986316ccd48596f506
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-23 15:37:31 +0900

    5.1.2 片方向のデータバインディング

---
+++ directive-oneway/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const inputNameBind = ref("しんちゃん");
+const inputNameOn = ref("ななし");
+const onInputName = (event: Event) => {
+  const element = event.target as HTMLInputElement;
+  inputNameOn.value = element.value;
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputNameBind"></input>
+  </section>
+  <br></br>
+  <section>
+    <input type="text" v-on:input="onInputName"></input>
+    <p>{{ inputNameOn }}</p>
+  </section>
+</template>
+
+<style scoped></style>

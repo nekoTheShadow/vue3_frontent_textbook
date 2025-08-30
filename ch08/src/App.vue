14c7c81009ff068387eccf38286e81b961de3f67
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 13:12:57 +0900

    8.3.4 v-forとPropsとの組み合わせ

--- ch08/src/App.vue
+++ ch08/src/App.vue
@@ -3,6 +3,7 @@ import ComponentsBasics from './ComponentsBasics/App.vue'
 import ComponentsPropsBasics from './ComponentsPropsBasics/App.vue'
 import ComponentsVModel from './ComponentsVModel/App.vue'
 import ComponentsPropsBind from './ComponentsPropsBind/App.vue'
+import ComponentsPropsVFor from './ComponentsPropsVFor/App.vue'
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import ComponentsPropsBind from './ComponentsPropsBind/App.vue'
   <ComponentsVModel></ComponentsVModel>
   <ComponentsPropsBasics></ComponentsPropsBasics>
   <ComponentsPropsBind></ComponentsPropsBind>
+  <ComponentsPropsVFor></ComponentsPropsVFor>
 </template>
 
 <style scoped></style>
---
+++ ch08/src/ComponentsPropsVFor/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import OneInfo from './components/OneInfo.vue'
+
+interface Weather {
+  id: number
+  title: string
+  content: string
+}
+
+const weathers = ref(
+  new Map<number, Weather>([
+    [1, { id: 1, title: '今日の天気', content: '今日は一日中晴でしょう' }],
+    [2, { id: 2, title: '明日の天気', content: '明日は一日中雨でしょう' }],
+    [3, { id: 3, title: '明後日の天気', content: '明後日は一日中雪でしょう' }],
+  ]),
+)
+</script>
+
+<template>
+  <h1>Props基礎</h1>
+  <section>
+    <h2>ループでコンポーネントを生成</h2>
+    <OneInfo
+      v-for="[id, weather] in weathers"
+      v-bind:key="id"
+      v-bind:title="weather.title"
+      v-bind:content="weather.content"
+    ></OneInfo>
+  </section>
+</template>
+
+<style lang="css" scoped>
+section {
+  border: blue;
+  margin: 10px;
+}
+</style>

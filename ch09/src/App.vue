87e55e11d662b323b18e58c1c5fbf11dfb83f892
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 17:52:52 +0900

    9.2 複数のSlotを実現する名前付きSlot

--- ch09/src/App.vue
+++ ch09/src/App.vue
@@ -3,6 +3,7 @@ import SlotBasic from './slot-basic/App.vue'
 import SlotFallback from './slot-fallback/App.vue'
 import SlotDatascope from './slot-datascope/App.vue'
 import SlotDynamic from './slot-dynamic/App.vue'
+import SlotNamed from './slot-named/App.vue'
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import SlotDynamic from './slot-dynamic/App.vue'
   <SlotFallback></SlotFallback>
   <SlotDatascope></SlotDatascope>
   <SlotDynamic></SlotDynamic>
+  <SlotNamed></SlotNamed>
 </template>
 
 <style scoped></style>
---
+++ ch09/src/slot-named/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import OneSection from './components/OneSection.vue'
+
+const taro = ref('田中太郎')
+const taroProblems = ref(['電話が通じません', '留守です'])
+const jiro = ref('鈴木次郎')
+</script>
+
+<template>
+  <section>
+    <h2>Slotの利用</h2>
+    <OneSection v-bind:name="taro">
+      <template v-slot:default>
+        <p>問題発生</p>
+      </template>
+      <template v-slot:detail>
+        <ul>
+          <li v-for="problem in taroProblems" v-bind:key="problem">{{ problem }}</li>
+        </ul>
+      </template>
+    </OneSection>
+    <OneSection v-bind:name="jiro"></OneSection>
+  </section>
+</template>

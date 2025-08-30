429c93ca45f00c7d36917b465383f3da21edcb04
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 17:47:28 +0900

    9.1.5 親でのレンダリング結果のSlot

--- ch09/src/App.vue
+++ ch09/src/App.vue
@@ -2,12 +2,14 @@
 import SlotBasic from './slot-basic/App.vue'
 import SlotFallback from './slot-fallback/App.vue'
 import SlotDatascope from './slot-datascope/App.vue'
+import SlotDynamic from './slot-dynamic/App.vue'
 </script>
 
 <template>
   <SlotBasic></SlotBasic>
   <SlotFallback></SlotFallback>
   <SlotDatascope></SlotDatascope>
+  <SlotDynamic></SlotDynamic>
 </template>
 
 <style scoped></style>
---
+++ ch09/src/slot-dynamic/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import OneSection from './components/OneSection.vue'
+
+const taro = ref('田中太郎')
+const taroProblems = ref(['電話が通じません', '留守です'])
+const jiro = ref('鈴木次郎')
+</script>
+
+<template>
+  <section>
+    <h2>Slotの利用</h2>
+    <OneSection v-bind:name="taro">
+      <ul>
+        <li v-for="problem in taroProblems" v-bind:key="problem">{{ problem }}</li>
+      </ul>
+    </OneSection>
+    <OneSection v-bind:name="jiro"></OneSection>
+  </section>
+</template>

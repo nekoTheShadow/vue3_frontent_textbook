830ec6c281a015d2786f49071b40d17663e49825
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 17:43:43 +0900

    9.1.4 テンプレート変数の所属先

--- ch09/src/App.vue
+++ ch09/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import SlotBasic from './slot-basic/App.vue'
 import SlotFallback from './slot-fallback/App.vue'
+import SlotDatascope from './slot-datascope/App.vue'
 </script>
 
 <template>
   <SlotBasic></SlotBasic>
   <SlotFallback></SlotFallback>
+  <SlotDatascope></SlotDatascope>
 </template>
 
 <style scoped></style>

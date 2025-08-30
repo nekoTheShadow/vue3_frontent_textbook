c83b5424c8eaf9b0cfeec00a81c03d0091898ea7
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 12:37:30 +0900

    8.1 子コンポーネントの利用

---
+++ ch08/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ComponentsBasics from './ComponentsBasics.vue'
+</script>
+
+<template>
+  <ComponentsBasics></ComponentsBasics>
+</template>
+
+<style scoped></style>

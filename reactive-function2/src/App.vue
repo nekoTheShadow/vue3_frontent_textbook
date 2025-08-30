961a89d2ed02a4ebbd7a5ad587d688628f1d21e1
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 11:48:42 +0900

    7.3.2 setupとreactiveとtoRefs

---
+++ reactive-function2/src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import {reactive, computed, defineComponent, toRefs} from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const data = reactive({PI:3.14, radius:Math.round(Math.random() * 10)});
+    const area = computed(() => data.radius * data.radius * data.PI);
+    setInterval(() => data.radius = Math.round(Math.random() * 10), 1000);
+    return {...toRefs(data), area}
+  },
+})
+
+
+</script>
+
+<template>
+  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
+</template>
+
+<style scoped></style>

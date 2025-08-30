2e58c1a82d1f5d0e584f3f2c06f893e3284363e5
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 11:05:50 +0900

    7.1.3 監視対象を明示するwatch()

--- ch07/src/App.vue
+++ ch07/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import ChangeCocktail from './ChangeCocktail.vue'
 import WatchEffect from './WatchEffect.vue'
+import Watch from './Watch.vue'
 </script>
 
 <template>
   <ChangeCocktail></ChangeCocktail>
   <WatchEffect></WatchEffect>
+  <Watch></Watch>
 </template>
 
 <style scoped></style>

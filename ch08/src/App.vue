9e8ecbd06e1195ee7d1f3252044a46df3a019028
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 13:05:32 +0900

    8.3.3 親のテンプレート変数をPropsに渡す方法

--- ch08/src/App.vue
+++ ch08/src/App.vue
@@ -2,12 +2,14 @@
 import ComponentsBasics from './ComponentsBasics/App.vue'
 import ComponentsPropsBasics from './ComponentsPropsBasics/App.vue'
 import ComponentsVModel from './ComponentsVModel/App.vue'
+import ComponentsPropsBind from './ComponentsPropsBind/App.vue'
 </script>
 
 <template>
   <ComponentsBasics></ComponentsBasics>
   <ComponentsVModel></ComponentsVModel>
   <ComponentsPropsBasics></ComponentsPropsBasics>
+  <ComponentsPropsBind></ComponentsPropsBind>
 </template>
 
 <style scoped></style>

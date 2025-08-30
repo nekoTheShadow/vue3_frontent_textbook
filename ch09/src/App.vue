1ab3500ee52d634a4477aabb26e288bcc059fa09
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 17:40:18 +0900

    9.1.3 Slotのフォールバックコンテンツ

--- ch09/src/App.vue
+++ ch09/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import SlotBasic from './slot-basic/App.vue'
+import SlotFallback from './slot-fallback/App.vue'
 </script>
 
 <template>
   <SlotBasic></SlotBasic>
+  <SlotFallback></SlotFallback>
 </template>
 
 <style scoped></style>

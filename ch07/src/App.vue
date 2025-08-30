1ecf4f82436e64dbae82ef098c8c3d4071856ad5
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 11:02:31 +0900

    7.1.2 リアクティブ変数の変化を監視するwatchEffect()

--- ch07/src/App.vue
+++ ch07/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ChangeCocktail from './ChangeCocktail.vue'
+import WatchEffect from './WatchEffect.vue'
 </script>
 
 <template>
   <ChangeCocktail></ChangeCocktail>
+  <WatchEffect></WatchEffect>
 </template>
 
 <style scoped></style>

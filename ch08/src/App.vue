60d4415f7c43d1bf7a14b65d558ff79b549f2925
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 12:45:51 +0900

    8.2 コンポーネントの独立性とCSSの扱い

--- ch08/src/App.vue
+++ ch08/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ComponentsBasics from './ComponentsBasics.vue'
+import ComponentsVModel from './ComponentsVModel.vue'
 </script>
 
 <template>
   <ComponentsBasics></ComponentsBasics>
+  <ComponentsVModel></ComponentsVModel>
 </template>
 
 <style scoped></style>

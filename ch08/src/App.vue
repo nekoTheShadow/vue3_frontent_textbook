234690daec15ad8b9108b765bf40ae02469b53b9
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-30 12:52:50 +0900

    8.3.1 親からデータをもらうProps

--- ch08/src/App.vue
+++ ch08/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import ComponentsBasics from './ComponentsBasics.vue'
+import ComponentsPropsBasics from './ComponentsPropsBasics.vue'
 import ComponentsVModel from './ComponentsVModel.vue'
 </script>
 
 <template>
   <ComponentsBasics></ComponentsBasics>
   <ComponentsVModel></ComponentsVModel>
+  <ComponentsPropsBasics></ComponentsPropsBasics>
 </template>
 
 <style scoped></style>

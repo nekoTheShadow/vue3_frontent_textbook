6d75d9f928d4d7edaccf6e49aafab59c93db7b51
neko_the_shadow <h.nakamura0903@gmail.com>
2025-08-31 14:01:10 +0900

    13.1 ユニットテストとVitest

---
+++ utest-basic/src/__tests__/functions.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from 'vitest'
+import { divideTwoNums } from '@/functions'
+
+describe('functions.tsのdivideTwoNumsのテスト', () => {
+  test('単純な割り算の場合のテスト', () => {
+    const num1 = 6
+    const num2 = 3
+    const actual = divideTwoNums(num1, num2)
+    const expected = 2
+    expect(actual).toBe(expected)
+  })
+  test('分子が0のテスト', () => {
+    const num1 = 0
+    const num2 = 3
+    const actual = divideTwoNums(num1, num2)
+    const expected = 0
+    expect(actual).toBe(expected)
+  })
+  test('分母が0のテスト', () => {
+    const num1 = 6
+    const num2 = 0
+    const actual = divideTwoNums(num1, num2)
+    const expected = Infinity
+    expect(actual).toBe(expected)
+  })
+})
